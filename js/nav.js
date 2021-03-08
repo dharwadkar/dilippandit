1c1c7a9639dd8891f9801537a9fdae52b3b26c51
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-07 23:31:01 -0800

    Fonts

--- js/nav.js
+++ js/nav.js
@@ -10,35 +10,24 @@ var loc = String(this.location);
 loc = loc.split("/");
 if (loc.length === 4) {
     loc1 = loc[loc.length - 1];
-    //console.log("Loc1 is: ", loc1)
     loc = loc[loc.length - 1].split(".")
     loc = loc[0]
-        //console.log(loc)
 } else {
     loc1 = loc[loc.length - 2];
-    //console.log("Loc1 is: ", loc1);
     loc2 = loc[loc.length - 1].split(".");
     loc2 = loc2[0];
-    //console.log("Loc2 is: ", loc2);
     loc = loc1 + "/" + loc2;
-    //console.log("Location is: ", loc);
 }
-/*for (var i = 0; i < loc.length; i++) {
-    console.log("Location", i, "is: ", loc[i]);
-}
-loc = loc[loc.length - 1].split(".");
-loc = loc[loc.length - 2];
-console.log("Location 2", loc); */
 
 //Menu generating function
 function dyn_menu_gen() {
     for (var i = 0; i < links.length; i++) {
         if (loc === links[i]) {
-            document.write('<li class=\"btn active\"><a href = \"#\">' + links_text[i] + '</a></li>');
+            document.write('<li class=\"btn active\"><a class="navi" href = \"#\">' + links_text[i] + '</a></li>');
         } else {
             //console.log("Links are: ", links[i]);
             //console.log("Links URL: ", links_url[i]);
-            document.write('<li class=\"btn\"><a href=\"' + links_url[i] + '\">' + links_text[i] + '</a></li>');
+            document.write('<li class=\"btn\"><a class="navi" href=\"' + links_url[i] + '\">' + links_text[i] + '</a></li>');
         }
         //document.write('<table.cellspacing="0" cellpadding="0" bgcolor="#FFFFFF"><tr><td></td></tr></table>');
     }
