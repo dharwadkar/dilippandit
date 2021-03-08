49cf654fab629d01d54b6275235082b0a81fd94d
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-07 19:31:05 -0800

    d

--- js/nav.js
+++ js/nav.js
@@ -2,7 +2,7 @@
 
 var links = new Array("tripreps/index", "stories/index", "birdalbum", "birdwatch", "delhibirds", "contactus", "faq", "aboutme");
 var links_text = new Array("Trip Reports", "Stories", "Bird Album", "Bird Watching", "Birds around Delhi", "Contact Us", "FAQ", "About Me");
-var links_url = new Array("/tripreps/index.html", "stories/index.html", "/birdalbum.html", "birdwatch.html", "delhibirds.html", "contactus.html", "faq.html", "aboutme.html");
+var links_url = new Array("/tripreps/index.html", "/stories/index.html", "/birdalbum.html", "/birdwatch.html", "/delhibirds.html", "/contactus.html", "/faq.html", "/aboutme.html");
 //Resolve the location
 
 var loc = String(this.location);
@@ -10,18 +10,18 @@ var loc = String(this.location);
 loc = loc.split("/");
 if (loc.length === 4) {
     loc1 = loc[loc.length - 1];
-    console.log("Loc1 is: ", loc1)
+    //console.log("Loc1 is: ", loc1)
     loc = loc[loc.length - 1].split(".")
     loc = loc[0]
-    console.log(loc)
+        //console.log(loc)
 } else {
     loc1 = loc[loc.length - 2];
-    console.log("Loc1 is: ", loc1);
+    //console.log("Loc1 is: ", loc1);
     loc2 = loc[loc.length - 1].split(".");
     loc2 = loc2[0];
-    console.log("Loc2 is: ", loc2);
+    //console.log("Loc2 is: ", loc2);
     loc = loc1 + "/" + loc2;
-    console.log("Location is: ", loc);
+    //console.log("Location is: ", loc);
 }
 /*for (var i = 0; i < loc.length; i++) {
     console.log("Location", i, "is: ", loc[i]);
@@ -36,8 +36,8 @@ function dyn_menu_gen() {
         if (loc === links[i]) {
             document.write('<li class=\"btn active\"><a href = \"#\">' + links_text[i] + '</a></li>');
         } else {
-            console.log("Links are: ", links[i]);
-            console.log("Links URL: ", links_url[i]);
+            //console.log("Links are: ", links[i]);
+            //console.log("Links URL: ", links_url[i]);
             document.write('<li class=\"btn\"><a href=\"' + links_url[i] + '\">' + links_text[i] + '</a></li>');
         }
         //document.write('<table.cellspacing="0" cellpadding="0" bgcolor="#FFFFFF"><tr><td></td></tr></table>');
