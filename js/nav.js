191c700ccb02d020050d9db66bd7c911ba05a988
Mukul Dharwadkar <dharmuku@amazon.com>
2021-03-10 00:34:34 -0800

    U

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,8 @@
 //Define the arrays for the links location
 
-var links = new Array("tripreps/", "stories/", "contactus", "aboutme");
-var links_text = new Array("Trip Reports", "Stories", "Contact Us", "About Me");
-var links_url = new Array("/tripreps/", "/stories/", "/contactus.html", "/aboutme.html");
+var links = new Array("index", "tripreps/", "stories/", "contactus", "aboutme");
+var links_text = new Array("Home", "Trip Reports", "Stories", "Contact Us", "About Me");
+var links_url = new Array("/index.html", "/tripreps/", "/stories/", "/contactus.html", "/aboutme.html");
 /*var links = new Array("tripreps/", "stories/", "birdalbum", "birdwatch", "delhibirds", "contactus", "faq", "aboutme");
 var links_text = new Array("Trip Reports", "Stories", "Bird Album", "Bird Watching", "Birds around Delhi", "Contact Us", "FAQ", "About Me");
 var links_url = new Array("/tripreps/", "/stories/", "/birdalbum.html", "/birdwatch.html", "/delhibirds.html", "/contactus.html", "/faq.html", "/aboutme.html");*/
